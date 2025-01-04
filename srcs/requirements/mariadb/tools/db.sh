b2abefae02652352b3ae7b15b1d4be5c8d67c239
AToufik01 <simotaoufiki99@gmail.com>
2025-01-04 09:28:21 -0500

    update nginx and wordpress

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
 service mariadb start
-
+sleep 5
 mysql -u root --password=${PASSWORD_ROOT}<<EOF
-CREATE DATABASE ${NAME_DATABASE};
-CREATE USER '${NAME_USER}'@'localhost' IDENTIFIED BY '${USER_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${NAME_DATABASE};
+CREATE USER IF NOT EXISTS '${NAME_USER}'@'localhost' IDENTIFIED BY '${USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${NAME_DATABASE}.* TO '${NAME_USER}'@'localhost';
 FLUSH PRIVILEGES;
 EOF
+tail -f /dev/null 
+#####
+#???#
+#####
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,1 +1,7 @@
-service php7.4-fpm start
+#!/bin/bash
+mkdir -p /run/php
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
+php-fpm7.4 -F
+
+
+# tail -f /dev/null 
